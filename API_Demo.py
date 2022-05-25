d3bae5b3d989f8d3dfe189ceadd45b2a077999c8
Adil Gajraj <adilgajraj@gmail.com>
2022-05-24 17:13:00 -0800

    first commit

---
+++ API_Demo.py
@@ -0,0 +1,23 @@
+# assign url to variable
+
+# Check response status of url
+# print response
+
+# print content / response.text
+
+# assign response object ariable (json loads)
+
+# print the clean data
+
+# create a workbook
+# create a spreadsheet page
+
+# designate columns
+
+# Loop through the columns
+    # Change the font to bold
+
+# Loop through data from API
+    # fill the cell values with appropriate data
+
+# Save the workbook
